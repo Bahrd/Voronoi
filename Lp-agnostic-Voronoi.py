17ca45c1c738d60fa32527e281b1e528f1591120
Przemysław Śliwiński, Ph.D., D.Sc <Przemyslaw.M.Sliwinski@windowslive.com>
2022-01-24 07:37:31 +0100

    Planted without seeds

--- Lp-agnostic-Voronoi.py
+++ Lp-agnostic-Voronoi.py
@@ -18,16 +18,21 @@ from VoronoiUtilities import *
 rsd = int(RA(0x12345678))
 c, sd = (int(argv[1]), int(argv[2])) if len(argv) == 3 else (int(argv[1]), rsd) if len(argv) == 2 else (0x10, rsd)
 
+patterns = [[0x60, 0x60, 0xb0, 0xb0, 0xb0], [0x60, 0xb0, 0x64, 0x60, 0xb0], 
+            [0xff, 0xff, 0, 0x80, 0], [0, 0xff, 0, 0x80, 0], [0, 0xff, 0, 0x80, 0]]
+
+
 # Lp, for both p and q
 w = 0x100
-qs, ps = [.25] , [.25, 1.0, 2.0, 4.0] # (2.0, 0.25, 1.0, 2.0, 4.0, 8.0) ## extensions = ('PNG', 'PDF')
+qs, ps = [.25] , [.25, 0.5, 1.0, 2.0, 4.0] # (2.0, 0.25, 1.0, 2.0, 4.0, 8.0) ## extensions = ('PNG', 'PDF')
 for p in ps:
    for q in qs: 
         # The reference diagrams for p and q...
-        NXY, _ = lp_Voronoi_diagram(w, p, c, sd), lp_Voronoi_diagram(w, q, c, sd)
+        _ = lp_planted_Voronoi_diagram(patterns, w, p)
 
+        #NXY, _ = lp_Voronoi_diagram(w, p, c, sd), lp_Voronoi_diagram(w, q, c, sd)
         # ... together with the diagram's Lp-agnostic counterparts w.r.t. p and q
-        lp_agnostic_Voronoi_diagram(*NXY, w, p, q, c, sd)
+        #lp_agnostic_Voronoi_diagram(*NXY, w, p, q, c, sd)
 
 # ... a summary and fanfares!
 for pl in ((0x1b8, 0x7d), (0x1b8, 0x7d), (0x19f, 0x7d), (0x1b8, 0xfa)): beep(*pl)

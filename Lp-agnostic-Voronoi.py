9d61b165070583450b8549d3ccb3c5fd2c327efa
Przemysław Śliwiński, Ph.D., D.Sc <Przemyslaw.M.Sliwinski@windowslive.com>
2022-02-08 22:30:15 +0100

    Extraordinary...

--- Lp-agnostic-Voronoi.py
+++ Lp-agnostic-Voronoi.py
@@ -4,7 +4,7 @@
 # 3. Compute an N × N Cartesian product of pattern's coordinates to get a Hanan grid
 # 4. Associate the new N × (N - 1) patterns to the classes w.r.t. the selected Lp
 # 5. Generate o Voronoi diagram for these N × N patterns (effectively, for the N × N Hanan grid)
-# 6. Repeat the steps #2-#5 for other Lq, 0 < q ≤ 2 (q = 0.25 seems arbitrary enough)
+# 6. Repeat the steps #2-#5 for other Lq, 0 < q ≤ 2 (q = 0.25 seems extraordinary enough)
 
 ## &"C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python37_64\python.exe" - YPMV (Your Python May Vary)
 ## .\Lp-agnostic-Voronoi.py 8 98828880 - used in the IEEE article
